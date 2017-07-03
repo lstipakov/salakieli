bb64d57dc4ebf03733d9078be56a297e460db694
Lev Stipakov <lstipakov@gmail.com>
2017-07-03 18:04:24 +0300

    Implement HTTP functionality

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"os"
+	"log"
+	"net/http"
 
 	"github.com/lstipakov/salakieli/renderer"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	text := "salakieli"
+
+	if err := r.ParseForm(); err == nil {
+		val, ok := r.Form["text"]
+		if ok {
+			text = val[0]
+		}
+	}
+
+	renderer.Render(text, w)
+}
+
 func main() {
-	renderer.Render("zlata", os.Stdout)
+	http.HandleFunc("/salakieli", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
